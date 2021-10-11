db7ef27be946ec94bb79e8e0dfe11310be51f644
csfx-py <csfxshreyas@gmail.com>
2021-10-12 05:17:49 +0530

    added db connection

---
+++ db/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+export function query(text, params) {
+  return pool.query(text, params);
+}
