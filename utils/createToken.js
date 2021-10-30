247bbbfe22c2225b7148410c57a69b9713d3427b
csfx-py <csfxshreyas@gmail.com>
2021-10-31 02:31:05 +0530

    added domains to token

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (user, secret, exp) => {
-  const { name, roles_id, is_priviledged } = user;
-  console.log(user);
+  const { name, roles_id, is_priviledged, shops_id } = user;
   return jwt.sign(
     {
       name,
       roles_id,
       is_priviledged,
+      shops_id,
     },
     secret,
     { expiresIn: exp }
